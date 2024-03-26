90255e2f1e0ac4ed03075e3b5eabefb114324d98
dhairyaaprajapati <127484178+dhairyaaprajapati@users.noreply.github.com>
2024-03-26 10:09:39 +0000

    terraform

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-007020fd9c84e18c7"
+  key_name      = "enginx-key"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "Ec2-Terra"
+  }
+
+}
